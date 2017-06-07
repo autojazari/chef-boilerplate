dd477f046792f3bfdcb1b7761b474abdd4baac30
Sameh Mohamed <smohamed1@lenovo.com>
2017-06-07 09:42:25 -0400

    added boilerpate recipes for stig

---
+++ config/solo.rb
@@ -0,0 +1,7 @@
+cookbook_path    ['/root/chef-boilerplate/cookbooks']
+role_path        '/root/chef-boilerplate/roles'
+environment_path '/root/chef-boilerplate/environments'
+node_name        'cistest.lenovosoftware.lenovo.com'
+environment      'dev'
+log_level        :info
+log_location     STDOUT

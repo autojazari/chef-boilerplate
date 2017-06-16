40477b8d1d01f336ddd19524b5f82a41b7df03e3
Sameh Mohamed <smohamed1@lenovo.com>
2017-06-16 10:58:48 -0400

    fixing sysctl

--- cookbooks/stig/recipes/proc_hard.rb
+++ cookbooks/stig/recipes/proc_hard.rb
@@ -37,8 +37,8 @@ commands = [
   'sysctl -w net.ipv4.conf.default.accept_redirects=0',
   'sysctl -w net.ipv4.conf.all.accept_source_route=0',
   'sysctl -w net.ipv4.conf.default.accept_source_route=0',
-  'sysctl -w net.ipv6.conf.all.accept_ra=0',
-  'sysctl -w net.ipv6.conf.default.accept_ra=0',
+  #'sysctl -w net.ipv6.conf.all.accept_ra=0',
+  #'sysctl -w net.ipv6.conf.default.accept_ra=0',
   'sysctl -w net.ipv4.ip_forward=0',
   'sysctl -w net.ipv4.route.flush=1']
 
